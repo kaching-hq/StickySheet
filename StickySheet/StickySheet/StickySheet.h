d6106833a60f97058154f8bd52902378929ae587
Reda Lemeden <git@redalemeden.com>
2019-11-07 17:38:57 +0100

    Add shared framework scheme

---
+++ StickySheet/StickySheet/StickySheet.h
@@ -0,0 +1,19 @@
+//
+//  StickySheet.h
+//  StickySheet
+//
+//  Created by Reda Lemeden on 2019-11-07.
+//  Copyright © 2019 Kaching AB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StickySheet.
+FOUNDATION_EXPORT double StickySheetVersionNumber;
+
+//! Project version string for StickySheet.
+FOUNDATION_EXPORT const unsigned char StickySheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickySheet/PublicHeader.h>
+
+
